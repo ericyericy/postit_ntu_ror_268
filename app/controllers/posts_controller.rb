5098cb7e5d532b02812ea6d4c92dc7f40ff40721
ericy <ericyericy@gmail.com>
2016-09-04 15:55:14 +0800

    add new action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,4 @@
+# require 'pry'
 class PostsController < ApplicationController
 	def index
 		@posts = Post.all
@@ -6,4 +7,29 @@ class PostsController < ApplicationController
 	def show
 		@post = Post.find(params[:id])
 	end
+
+	def new
+		@post = Post.new
+	end 
+
+	def create
+	  #前端使用者傳回的資料，建立一個新的 post 物件
+	  post = Post.new(post_params)
+	  
+	  #如果成功存進資料庫，就導回 index 頁面，失敗就再顯示一次表單
+	  if post.save!
+	    redirect_to posts_path
+	  else
+	    render :new
+	  end
+	end
+
+	private
+
+	#確保 params 裡面的 post hash 存在，並且允許 title 和 content 被存取
+	def post_params
+		# binding.pry
+	  params.require(:post).permit(:title, :content)
+	end	
+
 end
